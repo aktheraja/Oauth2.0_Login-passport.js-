0ff54dd0e8f510e162e0690c569649acd6d57178
aktheraja <wale.akin2007@gmail.com>
2020-01-12 21:45:02 -0600

    creating profile added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require ('passport');
+const profileRoutes = require('./routes/profile-routes');
+
 
 ////set up view engine
 app.set('view engine','ejs');
@@ -29,6 +31,7 @@ mongoose.connect(keys.mongodb.dbURl,()=>{
 
 //set up routes
 app.use('/auth',authRoutes);
+app.use('/profile',profileRoutes);
 
 //create home route
 app.get('/',(req,res)=>{
